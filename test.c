565f1d0e428ce76896ce7cc92f0d7e06ff3785be
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-06-21 18:35:36 +0100

    need fix more pipes

--- test.c
+++ test.c
@@ -27,31 +27,60 @@ int	ft_lstsize(t_data *lst)
 	return (len);
 }
 
+void    for_all(t_data  *new, int *pipe, int cmd_num, char **envp)
+{
+    pid_t   pid;
+    int     i;
+
+    i = 0;
+    pid = fork();
+    if (pid < 0)
+        exit(0);
+    if (pid == 0)
+    {
+        if (cmd_num == 0)
+        {
+            dup2(new->infile, 0);
+            dup2(pipe[1], 1);
+        }
+        else if(cmd_num == ft_lstsize(new) - 1)
+        {
+            dup2(pipe[2 * cmd_num - 2], 0);
+            dup2(new->outfile, 1);
+        }
+        else
+        {
+            dup2(pipe[2 * cmd_num - 2], 0);
+            dup2(pipe[2 * cmd_num + 1], 1);
+        }
+        while (i < ft_lstsize(new))
+            close(pipe[i++]);
+        exec(new->av, envp);
+    }
+}
+
 void execution(t_data *new, char **envp)
 {
-    dup2(new->infile, STDIN_FILENO);
-    if (!new->next)
+    int     *cannel;
+    int     i;
+
+    i = 0;
+    cannel =  (int *)malloc(sizeof(int) * (2 * ft_lstsize(new) - 1));
+    while (i < ft_lstsize(new) - 1)
+        if  (pipe(cannel + 2 * i++) < 0)
+            exit(0);
+    i = 0;
+    while (new)
     {
-        dup2(new->outfile, STDOUT_FILENO);
-        exec(new->av, envp);
-        return ;
-    }else if (ft_lstsize(new) == 2)
-        pipex1(new->av, new->next->av, envp);
-    else
-    {
-        while (new->next)
-        {
-            pipex(new->av, envp);
-            new = new->next;
-        }
-        exec(new->av, envp);
+        for_all(new, cannel, i++, envp);
+        new = new->next;
     }
 }
 
 int main(int argc, char **argv, char **envp)
 {
     t_data *new;
-    char    *stock;
+    // char    *stock;
     // int main_fork;
     char **new_envp;
 
@@ -62,7 +91,7 @@ int main(int argc, char **argv, char **envp)
         // stock = readline("$ ");
         new_envp = environment(envp);
         new = struct_args("ls", 0, 1);
-        // new->next = struct_args("ls", 0, open("a", O_RDONLY));
+        // new->next = struct_args("sort", 0, 1);
         // new->next->next = struct_args("top", 0, 1);
         // if (!ft_strncmp(stock, "exit", 5))
         //     exit(1);
@@ -70,9 +99,10 @@ int main(int argc, char **argv, char **envp)
         // if (!main_fork)
         // {
             execution(new, new_envp);
+            // exec(new->av, new_envp);
             // exit(1);
         // }
-        // waitpid(main_fork, NULL, 0);
+        // waitpid(0, 0, 0);
     // }
     free(new);
     free(new_envp);
