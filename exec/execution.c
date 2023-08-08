280d2043751379de5b14359215acc36aa9f61488
Mohcine Ghalmi <mghalmi@e2r9p14.1337.ma>
2023-08-08 13:29:41 +0100

    fix seg status

--- exec/execution.c
+++ exec/execution.c
@@ -6,7 +6,7 @@
 /*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 01:10:42 by selhilal          #+#    #+#             */
-/*   Updated: 2023/08/08 05:38:23 by mghalmi          ###   ########.fr       */
+/*   Updated: 2023/08/08 13:28:22 by mghalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,12 @@ void	update_and_wait(int ifcond, int status, t_env *envp)
 			ft_putstr_fd("\n", 1);
 		else if (WTERMSIG(status) == 3)
 			ft_putstr_fd("Quit: 3\n", 1);
+		if (WTERMSIG(status) == 11)
+		{
+			ft_putstr_fd("Segmentation fault: ", 2);
+			ft_putstr_fd(ft_itoa_shlvl(WTERMSIG(status)), 2);
+			ft_putstr_fd("\n", 2);
+		}
 		update_status(128 + WTERMSIG(status), envp, 1);
 	}
 	else if (ifcond < 2)
